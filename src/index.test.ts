89fdb8ecfe59a8abcb35c374a8e57cf2b1261f54
Max <golergka@gmail.com>
2021-04-21 02:27:06 +0300

    Basic transaction tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,34 +1,67 @@
-import { Pool } from "pg"
-import { tx } from "."
+import { Pool } from 'pg'
+import { tx } from '.'
 
 describe(`tx`, () => {
-  let pg: Pool
+	let pg: Pool
 
-  beforeAll(async () => {
-    const { POSTGRES_URL } = process.env
+	beforeAll(async () => {
+		const { POSTGRES_URL } = process.env
 
-    if (!POSTGRES_URL) {
-      throw new Error('Must specify POSTGRES_URL')
-    }
+		if (!POSTGRES_URL) {
+			throw new Error('Must specify POSTGRES_URL')
+		}
 
-    pg = new Pool({ connectionString: POSTGRES_URL })
+		pg = new Pool({ connectionString: POSTGRES_URL })
 
-    await pg.query(`CREATE TABLE things (
+		await pg.query(`CREATE TABLE things (
       id SERIAL PRIMARY KEY,
       thing TEXT NOT NULL
     )`)
-  })
-  afterAll(async () => {
-    await pg.query(`DROP TABLE THINGS`)
-    await pg.end()
-  })
+	})
+	afterAll(async () => {
+		await pg.query(`DROP TABLE THINGS`)
+		await pg.end()
+	})
 
-  it(`commits changes when there's no error`, async () => {
-    await tx(pg, async (db) => {
-      db.query(`INSERT INTO things (thing) VALUES ('comitted')`)
-    })
-    const {rows: [committed]} = await pg.query(`SELECT id, thing FROM things WHERE thing = 'comitted'`)
-    expect(committed).toBeDefined()
-    expect(committed.thing).toEqual('comitted')
-  })
+	it(`commits changes when there's no error`, async () => {
+		await tx(pg, async (db) => {
+			db.query(`INSERT INTO things (thing) VALUES ('comitted')`)
+		})
+
+		const {
+			rows: [committed]
+		} = await pg.query(`SELECT id, thing FROM things WHERE thing = 'comitted'`)
+		expect(committed).toBeDefined()
+		expect(committed.thing).toEqual('comitted')
+	})
+
+	it(`doesn't commit changes when there's a Node exception`, async () => {
+		await expect(
+			tx(pg, async (db) => {
+				await db.query(`INSERT INTO things (thing) VALUES ('node_error')`)
+				throw new Error(`node error`)
+			})
+		).rejects.toThrowError(`node error`)
+
+		const { rowCount } = await pg.query(
+			`SELECT id, thing FROM things WHERE thing = 'node_error'`
+		)
+		expect(rowCount).toBe(0)
+	})
+
+	it(`doesn't commit changes when there's a query error`, async () => {
+		await expect(
+			tx(pg, async (db) => {
+				await db.query(`INSERT INTO things(thing) VALUES ('query_error')`)
+				await db.query(`this query has an error`)
+			})
+		).rejects.toThrowErrorMatchingInlineSnapshot(
+			`"syntax error at or near \\"this\\""`
+		)
+
+		const { rowCount } = await pg.query(
+			`SELECT id, thing FROM things WHERE thing = 'query_error'`
+		)
+		expect(rowCount).toBe(0)
+	})
 })
