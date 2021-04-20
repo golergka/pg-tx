124bafe310c42b929146c17fee36f3b40d00fdd6
Max <golergka@gmail.com>
2021-04-21 02:08:07 +0300

    Naive implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('test')
+import { Pool, PoolClient } from 'pg'
+
+export async function tx<T>(
+	pg: Pool,
+	callback: (db: PoolClient) => Promise<T>
+): Promise<T> {
+	const client = await pg.connect()
+	await client.query(`BEGIN`)
+
+	try {
+		const result = await callback(client)
+		await client.query(`COMMIT`)
+		return result
+	} catch (e) {
+		await client.query(`ROLLBACK`)
+		throw e
+	} finally {
+		client.release()
+	}
+}
